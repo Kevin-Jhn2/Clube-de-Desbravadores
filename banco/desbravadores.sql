bfe99e3d90095fd66e32cf5a38df8bb49a570035
Kevin-Jhn2 <kevin.moreira@sptech.school>
2026-05-22 14:21:15 -0300

    Explicação para ajudar a implementar a API

--- banco/desbravadores.sql
+++ banco/desbravadores.sql
@@ -59,3 +59,111 @@ create table Presenca (
 );
 desc Presenca;
 
+
+-- Beleza, entendi! Vou te trazer um contexto: o meu projeto é sobre um site de gerenciar reuniões do Clube de Desbravadores. Daí tem um banco de dados como: create database Desbravadores;
+
+
+
+-- -- drop database Desbravadores; -- Não esquecer de tirar
+
+
+
+-- use Desbravadores;
+
+-- select * from Cadastro;
+
+-- -- drop table Cadastro;
+
+-- create table Cadastro(
+
+--     id int primary key auto_increment,
+
+--     nome varchar(200),
+
+--     email varchar(200),
+
+--     senha varchar(256),
+
+--     nomeClube varchar(200)
+
+-- );
+
+-- desc Cadastro;
+
+-- insert into Unidade values 
+
+-- (null, 'unidade a', 1);
+
+-- create table Unidade (
+
+--     id int primary key auto_increment,
+
+--     nome varchar(200),
+
+--     fkCadastro int,
+
+--     constraint fk_cadastro foreign key (fkCadastro)
+
+--     references Cadastro (id)
+
+-- );
+
+-- desc Unidade;
+
+
+
+-- create table Membro (
+
+--     id int primary key auto_increment,
+
+--     nome varchar(200),
+
+--     pontos int,
+
+--     fkUnidade int,
+
+--     constraint fk_unidade foreign key (fkUnidade)
+
+--     references Unidade(id)
+
+    
+
+-- );
+
+-- desc Membro;
+
+
+
+-- create table Reuniao (
+
+-- id int primary key auto_increment,
+
+-- data_reuniao date
+
+-- );
+
+-- desc Reuniao;
+
+
+
+-- create table Presenca (
+
+--     fkReuniao int,
+
+--     fkMembro int,
+
+--     constraint fk_reuniao foreign key (fkReuniao)
+
+--     references Reuniao (id),
+
+--     constraint fk_membro foreign key (fkMembro)
+
+--     references Membro (id)
+
+-- );
+
+-- desc Presenca;
+
+
+
+-- E a minha ideia é usar a presença dos usuários adicionados para fazer um gráfico de presença média do Clube, além de outros tipos. Depois mando mais informações.
